1faf21e9bb6d24ff96ed83309fa7968715d7645f
choudharyanuj <anuj-choudhary@outlook.com>
2019-09-28 17:51:13 +0530

    W8 Project

--- week_8/pos/src/App.js
+++ week_8/pos/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route } from 'react-router-dom'
 import { Link } from 'react-router-dom';  
 import Bill from './component/bill.js';
+import Home from './component/home.js'
 class App extends React.Component {
   render() {
     return(
@@ -10,29 +11,16 @@ class App extends React.Component {
         <div>
           <React.Fragment>
             <nav className=" navbar navbar-dark bg-dark fixed-top">
-               <img src="http://www.apnapunjabparattha.com/ApnaPunjabParatha/logo.png" className="logo-img ml-5" alt = "logo"></img> 
-               <Link to="/bill" className="text-decoration-none text-light">Generate Bill</Link><br />  
-              {/* <Link to="/mac" className="text-decoration-none text-light">MAC</Link><br />
-              <Link to="/ipad" className="text-decoration-none text-light">iPad</Link><br />
-              <Link to="/iphone" className="text-decoration-none text-light">iPhone</Link><br />
-              <Link to="/watch" className="text-decoration-none text-light">Watch</Link><br />
-              <Link to="/tv" className="text-decoration-none text-light">TV</Link><br />
-              <Link to="/music" className="text-decoration-none text-light">Music</Link><br />
-              <Link to="/support" className="text-decoration-none text-light">Support</Link><br /> */}
-              
+              <a href="component/home.js"><img src="http://www.apnapunjabparattha.com/ApnaPunjabParatha/logo.png" className="logo-img ml-5" alt="logo"></img> </a>
+               <Link to="/" className="text-decoration-none text-light">Home</Link><br />
+              <Link to="/bill" className="text-decoration-none text-light">Generate Bill</Link><br />  
             </nav>
-            <Route path="/bill" exact component={Bill} />
-            {/* <Route path="/mac" component={MAC} />
-            <Route path="/ipad" component={IPAD} />
-            <Route path="/iphone" component={IPHONE} />
-            <Route path="/watch" exact component={WATCH} />
-            <Route path="/tv" component={TV} />
-            <Route path="/music" component={MUSIC} />
-            <Route path="/support" component={SUPPORT} /> */}
+            <Route path="/" exact component={Home} />
+            <Route path="/bill" component={Bill} />
           </React.Fragment>
         </div>
         {/* <div>
-          <Bill/>
+          <Home/>
         </div> */}
       </div>
     )
--- week_8/pos/src/component/bill.js
+++ week_8/pos/src/component/bill.js
@@ -1,27 +1,46 @@
 import React from 'react';
 // import './App.css';
+let dish_name = [];
+let quantity_arr = [];
+let rate_arr = [];
+let total_price = [];
+let total = 0;
+
 class Bill extends React.Component {
-     data_entry = () => {
-            let data = [];
-            let item_name = document.getElementById('item_name').value;
-            let quantity = document.getElementById('quantity').value;
-            let rate = document.getElementById('rate').value;
-            let total = document.getElementById('total').value;
-            data.push(item_name);
-            data.push(quantity);
-            data.push(rate);
-            data.push(total);
-}
+   
+    data_entry = (e) => {
+        let sum = 0;
+        let item_name = document.getElementById('item_name').value;
+        let quantity = document.getElementById('quantity').value;
+        let rate = document.getElementById('rate').value;
+        dish_name.push(item_name);
+        quantity_arr.push(quantity);
+        rate_arr.push(rate);
+        total = quantity * rate;
+        total_price.push(total);
+        // console.log(dish_name);
+        // console.log(quantity_arr);
+        // console.log(rate_arr);
+        // console.log(total_price);
+        // console.log(total); 
+        for(var i = 0; i < total_price.length; i++) {
+            sum = sum + total_price[i]
+        }
+        alert(
+            "Yor Odrde Was :-" + " " + item_name + " " + "Your Total Bill Ammount is :-" + " " + sum
+        )
+        console.log(sum); 
+    }
     render() {
         return (
-            <div className = "table-size">
-            <table className="table mt-5">
-                <thead className="thead-dark">
+            <div className = " Container mt-5">
+            <div className= "table-size bg-image">
+            <table className="table mt-5 table-size">
+                        <thead className="thead-dark mt-5 table-size" >
                     <tr>
                         <th scope="col">Name of the item</th>
                         <th scope="col">Quantity</th>
                         <th scope="col">Rate</th>
-                        <th scope="col">Total</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -29,12 +48,15 @@ class Bill extends React.Component {
                         <th scope = "row"><input type = "text" placeholder = "Item name" id = "item_name"></input></th>
                         <th scope = "row"><input type = "text" placeholder = "Quantity" id = "quantity"></input></th>
                         <th scope = "row"><input type = "text" placeholder = "Rate" id = "rate"></input></th>
-                        <th scope="row"><input type="text" placeholder="Total" id="total"></input></th>
                     </tr>
                 </tbody>
             </table>
-                <button onClick={() => this.data_entry()}>Submit</button>
+            <div className = "text-center">
+                <button onClick={() => this.data_entry()} className = "text-center">Submit</button>
             </div>
+            </div>
+            </div>
+            
         )
     }
 }
